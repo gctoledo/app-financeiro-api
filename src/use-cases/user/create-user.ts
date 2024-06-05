b9fd3d802e64aa92a1fb648299c7ff7d40e90497
Gabriel <gabrielcostatoledo@outlook.com>
2024-06-05 17:35:24 -0300

    feat: add CreateUserUseCase

---
+++ src/use-cases/user/create-user.ts
@@ -0,0 +1,32 @@
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+import { User, UserWId } from "../../schemas/user";
+import { PostgresGetUserByEmailRepository } from "../../repositories/postgres/user/get-user-by-email";
+import { PostgresCreateUserRepository } from "../../repositories/postgres/user/create-user";
+
+export class CreateUserUseCase {
+  async execute(params: User): Promise<UserWId> {
+    const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
+    const emailAlreadyExists = await getUserByEmailRepository.execute(
+      params.email
+    );
+    if (emailAlreadyExists) {
+      throw new Error("Email already exists");
+    }
+
+    const userId = uuidv4();
+
+    const hashedPassword = await bcrypt.hash(params.password, 10);
+
+    const user = {
+      ...params,
+      id: userId,
+      password: hashedPassword,
+    };
+
+    const postgresCreateUserRepository = new PostgresCreateUserRepository();
+    const createdUser = await postgresCreateUserRepository.execute(user);
+
+    return createdUser;
+  }
+}
