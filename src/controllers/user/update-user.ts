d8289e9a816797dcd02bef67d64fb43fd54c1d5f
Gabriel <gabrielcostatoledo@outlook.com>
2024-06-06 15:26:56 -0300

    feat: add UpdateUserController

---
+++ src/controllers/user/update-user.ts
@@ -0,0 +1,51 @@
+import { Request } from "express";
+import {
+  UserControllerResponse,
+  badRequest,
+  notFound,
+  ok,
+  serverError,
+} from "../helpers/http";
+import { validateId } from "../helpers/validation";
+import { updateUserSchema } from "../../schemas/user";
+import { UpdateUserUseCase } from "../../use-cases/user/update-user";
+import { EmailAlreadyInUseError, UserNotFoundError } from "../../errors/user";
+import { ZodError } from "zod";
+
+export class UpdateUserController {
+  async execute(httpRequest: Request): Promise<UserControllerResponse> {
+    try {
+      const { userId } = httpRequest.params;
+      const params = httpRequest.body;
+
+      const idIsValid = validateId(userId);
+
+      if (!idIsValid) {
+        return badRequest({ message: "Invalid user id" });
+      }
+
+      await updateUserSchema.parseAsync(params);
+
+      const updateUserUseCase = new UpdateUserUseCase();
+      const updatedUser = await updateUserUseCase.execute(params, userId);
+
+      return ok(updatedUser);
+    } catch (err) {
+      console.error(err);
+
+      if (err instanceof ZodError) {
+        return badRequest({ message: err.errors[0].message });
+      }
+
+      if (err instanceof UserNotFoundError) {
+        return notFound({ message: err.message });
+      }
+
+      if (err instanceof EmailAlreadyInUseError) {
+        return badRequest({ message: err.message });
+      }
+
+      return serverError();
+    }
+  }
+}
