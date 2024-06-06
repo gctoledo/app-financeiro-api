82fa1cf6ee9c9ef31cbc663945cdba56424821fb
Gabriel <gabrielcostatoledo@outlook.com>
2024-06-06 17:09:47 -0300

    feat: add CreateBalanceUseCase

---
+++ src/use-cases/balance/create-balance.ts
@@ -0,0 +1,35 @@
+import { Balance as PrismaBalance } from "@prisma/client";
+import { Balance } from "../../schemas/balance";
+import { PostgresGetUserByIdRepository } from "../../repositories/postgres/user/get-user-by-id";
+import { UserNotFoundError } from "../../errors/user";
+import { v4 as uuidv4 } from "uuid";
+import { Decimal } from "@prisma/client/runtime/library";
+import { PostgresCreateBalanceRepository } from "../../repositories/postgres/balance/create-balance";
+
+export class CreateBalanceUseCase {
+  async execute(params: Balance): Promise<PrismaBalance> {
+    const getUserByIdRepository = new PostgresGetUserByIdRepository();
+    const userExist = getUserByIdRepository.execute(params.user_id);
+
+    if (!userExist) {
+      throw new UserNotFoundError();
+    }
+
+    const balanceId = uuidv4();
+
+    const balance = {
+      ...params,
+      id: balanceId,
+      createdAt: new Date(params.createdAt),
+      credit_amount: new Decimal(params.credit_amount),
+      debit_amount: new Decimal(params.debit_amount),
+      cash_amount: new Decimal(params.cash_amount),
+      expense_amount: new Decimal(params.expense_amount),
+    };
+
+    const createBalanceRepository = new PostgresCreateBalanceRepository();
+    const createdBalance = await createBalanceRepository.execute(balance);
+
+    return createdBalance;
+  }
+}
