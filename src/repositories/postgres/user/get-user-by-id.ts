a0678f1b753c899729be83f3a5f3d99bc1fe8b76
Gabriel <gabrielcostatoledo@outlook.com>
2024-06-06 02:30:15 -0300

    refactor: ensure use cases throws UserNotFoundError

--- src/repositories/postgres/user/get-user-by-id.ts
+++ src/repositories/postgres/user/get-user-by-id.ts
@@ -1,18 +1,13 @@
 import { User } from "@prisma/client";
 import prisma from "../../../../prisma/prisma";
-import { UserNotFoundError } from "../../../errors/user";
 
 export class GetUserByIdRepository {
-  async execute(userId: string): Promise<User> {
+  async execute(userId: string): Promise<User | null> {
     const user = await prisma.user.findUnique({
       where: {
         id: userId,
       },
     });
-
-    if (!user) {
-      throw new UserNotFoundError();
-    }
 
     return user;
   }
--- src/use-cases/user/get-user-by-id.ts
+++ src/use-cases/user/get-user-by-id.ts
@@ -1,11 +1,16 @@
 import { User } from "@prisma/client";
 import { GetUserByIdRepository } from "../../repositories/postgres/user/get-user-by-id";
+import { UserNotFoundError } from "../../errors/user";
 
 export class GetUserByIdUseCase {
   async execute(userId: string): Promise<User> {
     const getUserByIdRepository = new GetUserByIdRepository();
 
     const user = await getUserByIdRepository.execute(userId);
+
+    if (!user) {
+      throw new UserNotFoundError();
+    }
 
     return user;
   }
