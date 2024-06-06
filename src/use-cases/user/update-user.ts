1e140ef735f6290143283d59c08b89a46f9baf19
Gabriel <gabrielcostatoledo@outlook.com>
2024-06-06 15:26:50 -0300

    feat: add UpdateUserUseCase

---
+++ src/use-cases/user/update-user.ts
@@ -0,0 +1,43 @@
+import { User } from "@prisma/client";
+import { PostgresGetUserByEmailRepository } from "../../repositories/postgres/user/get-user-by-email";
+import { UpdateUser } from "../../schemas/user";
+import { EmailAlreadyInUseError, UserNotFoundError } from "../../errors/user";
+import bcrypt from "bcrypt";
+import { PostgresUpdateUserRepository } from "../../repositories/postgres/user/update-user";
+import { PostgresGetUserByIdRepository } from "../../repositories/postgres/user/get-user-by-id";
+
+export class UpdateUserUseCase {
+  async execute(params: UpdateUser, userId: string): Promise<User> {
+    const getUserByIdRepository = new PostgresGetUserByIdRepository();
+    const userExist = await getUserByIdRepository.execute(userId);
+
+    if (!userExist) {
+      throw new UserNotFoundError();
+    }
+
+    if (params.email) {
+      const getUserByEmailRepository = new PostgresGetUserByEmailRepository();
+      const userWithProvidedEmail = await getUserByEmailRepository.execute(
+        params.email
+      );
+
+      if (userWithProvidedEmail && userWithProvidedEmail.id !== userId) {
+        throw new EmailAlreadyInUseError(params.email);
+      }
+    }
+
+    const user: UpdateUser = {
+      ...params,
+    };
+
+    if (params.password) {
+      const hashedPassword = await bcrypt.hash(params.password, 10);
+      user.password = hashedPassword;
+    }
+
+    const updateUserRepository = new PostgresUpdateUserRepository();
+    const updatedUser = await updateUserRepository.execute(user, userId);
+
+    return updatedUser;
+  }
+}
