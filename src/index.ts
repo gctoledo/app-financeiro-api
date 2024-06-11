3793c13fc16739cf8cad70b346b241399a45013a
Gabriel <gabrielcostatoledo@outlook.com>
2024-06-11 13:23:09 -0300

    feat: add cors to express routes

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import "dotenv/config.js";
 import express from "express";
 import userRouter from "./routes/user";
 import balanceRouter from "./routes/balance";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", userRouter, balanceRouter);
 
