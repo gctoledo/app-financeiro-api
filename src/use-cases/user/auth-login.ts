6d487ebc0b3fc350808a381c64cec29eafd1478a
Gabriel <gabrielcostatoledo@outlook.com>
2024-06-13 19:07:43 -0300

    fix: add first name to jwt payload

--- src/use-cases/user/auth-login.ts
+++ src/use-cases/user/auth-login.ts
@@ -24,6 +24,7 @@ export class AuthLoginUseCase {
       {
         id: user.id,
         email: user.email,
+        first_name: user.first_name,
       },
       process.env.JWT_SECRET as string,
       {
